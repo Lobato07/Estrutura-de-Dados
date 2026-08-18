b0ae572aa9dbdb215df339ef74d4db4e923a85d9
Lobato <lobatoamorim1@gmail.com>
2026-08-18 20:03:42 -0300

    18.08

--- Lista01/ex001.cc
+++ Lista01/ex001.cc
@@ -1,45 +1,13 @@
 #include <iostream>
 using namespace std;
-// int somar (int a, int b);
-// void dobra (float *a);
 
 int main(){
 
-    // cout << "1) Declare um inteiro, crie um ponteiro para ele e imprima o valor e o endereço." << endl;
-    // int a = 5, *pntr = &a;
+    cout << "1) Declare um inteiro, crie um ponteiro para ele e imprima o valor e o endereço." << endl;
+    int a = 5, *pntr = &a;
 
-    // cout << "Valor de A: " << a << endl;
-    // cout<<"Endereco na memoria: " << pntr << endl << endl;
-// ------------------------------------------------------------------------------------------------------
-
-    // cout<<"5) Crie uma funcao que some dois números usando uma funcao que recebe ponteiros e retorne o resultado como um ponteiro"<<endl;
-    // int f1 = 0, f2 = 0, f3 = 0, *pntr1 = &f1, *pntr2 = &f2, *pntr3 = &f3;
-
-    // cout<<"Digite o valor de A: ";
-    // cin >> *pntr1;
-    // cout<<"Digite o valor de B: ";
-    // cin >> *pntr2;
-
-    // f3 = somar(*pntr1, *pntr2);
-    
-    // cout<<"O resultado da soma eh: "<< *pntr3 << endl << endl;
-// ------------------------------------------------------------------------------------------------------
-    // cout<<"6) Crie uma funcao dobra que recebe um valor ponteiro de float e dobra seu valor retornando o valor em dobro na mesma variável"<<endl;
-    // float e, *pntre = &e;
-
-    // cout<<"Digite o valor de E: ";
-    // cin>>*pntre;
-
-    // dobra(pntre);
-
-    // cout<<"Valor de E duplicado: " << *pntre<< endl << endl;
-// ------------------------------------------------------------------------------------------------------
-    cout<<"8) Crie uma funcao que recebe 2 strings entrada e saida ambas ponteiros para char e inverte o texto de entrada em saída"<<endl;
-
+    cout << "Valor de A: " << a << endl;
+    cout<<"Endereco na memoria: " << pntr << endl << endl;
 
     return EXIT_SUCCESS;
 }
-
-// void dobra (float *a){
-//     *a = *a * 2;
-// }
--- Lista01/ex006.cc
+++ Lista01/ex006.cc
@@ -1,8 +1,21 @@
 #include <iostream>
 using namespace std;
+void dobra (float *a);
 
 int main(){
-    
+    cout<<"6) Crie uma funcao dobra que recebe um valor ponteiro de float e dobra seu valor retornando o valor em dobro na mesma variável"<<endl;
+    float e, *pntre = &e;
+
+    cout<<"Digite o valor de E: ";
+    cin>>*pntre;
+
+    dobra(pntre);
+
+    cout<<"Valor de E duplicado: " << *pntre<< endl << endl;  
     
     return EXIT_SUCCESS;
+}
+
+void dobra (float *a){
+    *a = *a * 2;
 }
