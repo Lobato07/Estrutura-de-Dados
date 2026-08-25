f3a81ee5d54ba7304c775dfbd31fe4a3b7d40cd2
Lobato-py <lobatoamorim1@gmail.com>
2026-08-25 20:51:42 -0300

    25.08

---
+++ Lista01/ex008.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void inverte(char* entrada, char* saida){
+    int tam = strlen(entrada); // Para usar o "strlen", preciso incluir o cstring
+    for (int i = 0; i < tam; i++){
+        saida[tam-1-i] = entrada[i];
+    }
+    saida[tam] = entrada[tam]; // copia o '\0'
+}
+
+int main(){
+    char entrada[100], saida [100];
+    cout<< "Digite a entrada: ";
+    cin >> entrada;
+
+    inverte(entrada,saida);
+    cout << "A saida invertida eh: " << saida;
+}
