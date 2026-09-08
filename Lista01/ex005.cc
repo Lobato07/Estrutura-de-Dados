42c5edc5cf73dd8dbd08301753d3f805d48a9ecb
lobato <lobatoamorim1@gmail.com>
2026-09-08 14:41:35 -0300

    08.09

--- Lista01/ex005.cc
+++ Lista01/ex005.cc
@@ -1,23 +1,29 @@
 #include <iostream>
+#include <locale>
 using namespace std;
-int somar (int a, int b);
 
-int main(){
-    cout<<"5) Crie uma funcao que some dois números usando uma funcao que recebe ponteiros e retorne o resultado como um ponteiro"<<endl;
-    int f1 = 0, f2 = 0, f3 = 0, *pntr1 = &f1, *pntr2 = &f2, *pntr3 = &f3;
-
-    cout<<"Digite o valor de A: ";
-    cin >> *pntr1;
-    cout<<"Digite o valor de B: ";
-    cin >> *pntr2;
-
-    f3 = somar(*pntr1, *pntr2);
-    
-    cout<<"O resultado da soma eh: "<< *pntr3 << endl << endl;
-    
-    return EXIT_SUCCESS;
+void *SomaPonteiros(int *a, int *b, int *resultado){
+    *resultado = *a + *b;
+    return resultado;
 }
 
-int somar (int a , int b){
-    return a + b;
+int main(){
+
+    locale::global(locale("pt_BR.UTF-8"));
+
+    // Crie uma função que some dois números usando uma função que recebe ponteiros e retorne o resultado como um ponteiro
+
+    int x, y, res;
+
+    cout << "Digite um valor: ";
+    cin >> x;
+    cout << "Digite um segundo valor: ";
+    cin >> y;
+
+    SomaPonteiros(&x, &y, &res);
+    cout << res << endl;
+
+
+    
+    return EXIT_SUCCESS;
 }
