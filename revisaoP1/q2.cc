c39cc1ba05eb499f9c5bf7558428bcb8f400ce26
Lobato-py <lobatoamorim1@gmail.com>
2026-09-14 18:31:18 -0300

    14.09

--- revisaoP1/q2.cc
+++ revisaoP1/q2.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 struct Ponto{
@@ -6,7 +7,7 @@ struct Ponto{
     int x;
     int y;
 
-}
+};
 
 int main(){
 
@@ -14,7 +15,25 @@ int main(){
 
     cout << "Digite o valor de X1: ";
     cin >> p1.x;
+    cout << "Digite o valor de Y1: ";
+    cin >> p1.y;
+    cout << "Digite o valor de X2: ";
+    cin >> p2.x;
+    cout << "Digite o valor de Y2: ";
+    cin >> p2.y;
+
+    float result = 0, xFinal = 0, yFinal = 0;
+
+    xFinal = p2.x - p1.x;
+    xFinal *= xFinal;
+
+    yFinal = p2.y - p1.y;
+    yFinal *= yFinal;
+
+    result = sqrt(xFinal+yFinal);
     
+    cout << "A distancia eh: " << result << endl;
+
 
     return EXIT_SUCCESS;
 }
