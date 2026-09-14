1c2a094a9d1f64d06e89ac8b0d640e388892bc13
lobato <lobatoamorim1@gmail.com>
2026-09-14 13:01:22 -0300

    14.09

---
+++ revisaoP1/q2.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+struct Ponto{
+
+    int x;
+    int y;
+
+}
+
+int main(){
+
+    Ponto p1, p2;
+
+    cout << "Digite o valor de X1: ";
+    cin >> p1.x;
+    
+
+    return EXIT_SUCCESS;
+}
