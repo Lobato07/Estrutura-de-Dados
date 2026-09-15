6fa652ec2379bbe785c8d328cec36ceb792712f3
Lobato-py <lobatoamorim1@gmail.com>
2026-09-14 21:56:04 -0300

    14.09

--- revisaoP1/q3.cc
+++ revisaoP1/q3.cc
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
 using namespace std;
 
 struct Produto{
     int id;
-    string nome;
-    float preco;
+    string name;
+    float price;
 };
 
 int main(){
+
+    Produto p1;
 
     int option = 0;
 
@@ -23,29 +27,92 @@ int main(){
         cin >> option;
 
         switch(option){
-            case 1:
-                cout << "Inserir produto" << endl;
+            case 1:{
+                ofstream archive("produtos.txt", ios::app);
+
+                cout << "-----------------------------------------------------------" << endl;
+                cout << "------ Inserir produto ------" << endl;
+                cout << "Digite o ID do produto: ";
+                cin >> p1.id;
+                cout << "Digite o nome do produto: ";
+                cin.ignore();
+                getline(cin, p1.name);
+                cout << "Digite o preco do produto: ";
+                cin >> p1.price;
+                cout << "-----------------------------------------------------------" << endl;
+
+                archive << p1.id << ";" << p1.name << ";" << p1.price << ";" << endl ;
+                archive.close();
                 break;
-            
-            case 2:
-                cout << "Listar todos" << endl;
+            }
+
+            case 2:{
+                ifstream archive("produtos.txt");
+
+                cout << "-----------------------------------------------------------" << endl;
+                cout << "------ Listar todos ------" << endl;
+                string linha;
+                while (getline(archive, linha)) {
+                    stringstream ss(linha);
+                    string idStr, nome, precoStr;
+                    getline(ss, idStr, ';');
+                    getline(ss, nome, ';');
+                    getline(ss, precoStr, ';');
+                    cout << idStr << " - " << nome << " - " << precoStr << endl;
+                }
+
+                cout << "-----------------------------------------------------------" << endl;
+                archive.close();
                 break;
-            
-            case 3:
-                cout << "Buscar por nome" << endl;
+            }
+
+            case 3:{
+                ifstream archive("produtos.txt");
+                string nomeBusca;
+
+                cout << "-----------------------------------------------------------" << endl;
+                cout << "------ Buscar por nome ------" << endl;
+                cout << "Digite o nome do produto a buscar: ";
+                cin.ignore();
+                getline(cin, nomeBusca);
+
+                string linha;
+                bool encontrado = false;
+                while (getline(archive, linha)) {
+                    stringstream ss(linha);
+                    string idStr, nome, precoStr;
+                    getline(ss, idStr, ';');
+                    getline(ss, nome, ';');
+                    getline(ss, precoStr, ';');
+
+                    if (nome == nomeBusca){
+                        cout << "- ID " << idStr << " " << nome << endl;
+                        encontrado = true;
+                    }
+                }
+
+                if (!encontrado){
+                    cout << "Produto nao encontrado." << endl;
+                }
+
+
+                cout << "-----------------------------------------------------------" << endl;
+                archive.close();
                 break;
-            
-            case 4:
+            }
+
+            case 4:{
                 cout << "Saindo..." << endl;
                 break;
+            }
 
-            default:
+            default:{
                 cout << "Valor invalido!" << endl;
                 break;
+            }
         }
 
     }while (option != 4);
     
-
     return EXIT_SUCCESS;
 }
